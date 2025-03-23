eda4ae71ed73bb8d60b22fd508e816e6693a38d2
leonardo-aragonez <leonardo.aragonez@quind.io>
2025-03-22 20:24:28 -0500

    se crea la estructura del proyecto

---
+++ src/main/java/com/service/ms/control_vehicular/control_vehicular/ControlVehicularApplication.java
@@ -0,0 +1,13 @@
+package com.service.ms.control_vehicular.control_vehicular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlVehicularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlVehicularApplication.class, args);
+	}
+
+}
