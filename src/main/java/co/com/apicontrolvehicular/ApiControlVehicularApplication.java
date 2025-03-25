2b107920b2d29b6c6efd3354c60105a4483a07e2
leonardo-aragonez <leonardo.aragonez@quind.io>
2025-03-25 13:08:28 -0500

    se ajusta la estructura

---
+++ src/main/java/co/com/apicontrolvehicular/ApiControlVehicularApplication.java
@@ -0,0 +1,13 @@
+package co.com.apicontrolvehicular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControlVehicularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiControlVehicularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/apicontrolvehicular/ApiControlVehicularApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.apicontrolvehicular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControlVehicularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
